8d1759ce613c7260dce723471e3b4122ebeaa72a
Tomas Valent <equivalent@eq8.eu>
2015-10-07 08:11:40 +0000

    move generate to own class + optional grid args

---
+++ lib/maze_magic/generate.rb
@@ -0,0 +1,77 @@
+module MazeMagic
+  class Generate
+    attr_reader :height, :width, :maze, :options
+    attr_writer :algorithm
+
+    def initialize(height:, width:, **options)
+      @height = height
+      @width  = width
+      @options = options
+    end
+
+    def generate_maze
+      initialize_instructions_grid
+      apply_maze_algorithm_to_grid
+      generate_maze_cells_from_instructions
+      return maze
+    end
+
+    def algorithm
+      @algorithm ||= MazeMagic::MazeGenerator::RecursiveBacktracking
+    end
+
+    private
+
+    attr_reader :instructions_grid
+
+    ## Initialize instructions grid
+    #
+    #        _________
+    #       |_|_|_|_|_|
+    #       |_|_|_|_|_|
+    #       |_|_|_|_|_|
+    #       |_|_|_|_|_|
+    #       |_|_|_|_|_|
+    #
+    def initialize_instructions_grid
+      grid_options = options.select { |k,v| %i(start_x start_y).include?(k) }
+      args = { height: height, width: width }.merge(grid_options)
+      @instructions_grid = grid_klass.new(args)
+    end
+
+    def apply_maze_algorithm_to_grid
+      MazeMagic::MazeGenerator::RecursiveBacktracking
+        .new(grid: instructions_grid)
+        .call
+    end
+
+    ## Generate Maze Cell objects from instructions:
+    #
+    #  2 dimensional array of these singleton objects:
+    #
+    #       MazeMagic::Edge.instance              # ' '
+    #       MazeMagic::HorizontalWall.instance    #  _
+    #       MazeMagic::VerticalWall.instance      #  |
+    #       MazeMagic::Passage.instance           # ' '
+    #
+    #  Objects inside can be translated to maze walls:
+    #
+    #        _________
+    #       |_  |  _  |
+    #       |  _| |  _|
+    #       | |_  | | |
+    #       | |  _| | |
+    #       |___|_____|
+    #
+    def generate_maze_cells_from_instructions
+      @maze ||= MazeMagic::MazeGenerator::InstructionsGridToCellsGrid
+        .new(grid: instructions_grid)
+        .tap { |generator| generator.call }
+        .cells_grid
+    end
+
+    def grid_klass
+      MazeMagic::Grid
+    end
+  end
+end
--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -1,86 +1,39 @@
 require 'spec_helper'
 
-module MazeMagic
-  class Generate
-    attr_reader :height, :width, :maze
-    attr_writer :algorithm
-
-    def initialize(height:, width:)
-      @height = height
-      @width  = width
-    end
-
-    def generate_maze
-      initialize_instructions_grid
-      apply_maze_algorithm_to_grid
-      generate_maze_cells_from_instructions
-      return maze
-    end
-
-    def algorithm
-      @algorithm ||= MazeMagic::MazeGenerator::RecursiveBacktracking
-    end
-
-    private
-
-    attr_reader :instructions_grid
-
-    ## Initialize instructions grid
-    #
-    #        _________
-    #       |_|_|_|_|_|
-    #       |_|_|_|_|_|
-    #       |_|_|_|_|_|
-    #       |_|_|_|_|_|
-    #       |_|_|_|_|_|
-    #
-    def initialize_instructions_grid
-      @instructions_grid = MazeMagic::Grid.new(height: height, width: width)
-    end
-
-    def apply_maze_algorithm_to_grid
-      MazeMagic::MazeGenerator::RecursiveBacktracking
-        .new(grid: instructions_grid)
-        .call
-    end
-
-    ## Generate Maze Cell objects from instructions:
-    #
-    #  2 dimensional array of these singleton objects:
-    #
-    #       MazeMagic::Edge.instance              # ' '
-    #       MazeMagic::HorizontalWall.instance    #  _
-    #       MazeMagic::VerticalWall.instance      #  |
-    #       MazeMagic::Passage.instance           # ' '
-    #
-    #  Objects inside can be translated to maze walls:
-    #
-    #        _________
-    #       |_  |  _  |
-    #       |  _| |  _|
-    #       | |_  | | |
-    #       | |  _| | |
-    #       |___|_____|
-    #
-    def generate_maze_cells_from_instructions
-      @maze ||= MazeMagic::MazeGenerator::InstructionsGridToCellsGrid
-        .new(grid: instructions_grid)
-        .tap { |generator| generator.call }
-        .cells_grid
-    end
-  end
-end
-
 RSpec.describe MazeMagic::Generate do
   include MazeExamples
 
-  subject { described_class.new(height: 1, width: 1) }
+  subject { described_class.new(args) }
 
-  before do
-    subject.generate_maze
+  context 'generating maze with regular arguments' do
+    let(:args) { { height: 1, width: 1 } }
+
+    before do
+      subject.generate_maze
+    end
+
+    it 'should generate array of maze objects' do
+      expect(subject.maze).to eq cells_grid_1by1
+    end
   end
 
-  it 'should generate array of maze objects' do
-    expect(subject.maze).to eq cells_grid_1by1
+  context 'generate maze when passing additional grid options' do
+    let(:grid_args) { { height: 1, width: 1, start_x: 1, start_y: 1 } }
+    let(:args) { grid_args.merge({ something_not: 'important' }) }
+    let(:grid_klass_spy) { spy }
+
+    before do
+      expect(subject)
+        .to receive(:grid_klass)
+        .and_return(grid_klass_spy)
+
+      subject.generate_maze
+    end
+
+    it 'should pass grid options to grid' do
+      expect(grid_klass_spy)
+        .to have_received(:new)
+        .with(grid_args)
+    end
   end
 end
